5cee6aa046e2034d1b20da970e8bab011ea5c91a
jwdukes <jwdukes@gmail.com>
2021-02-23 22:26:50 -0600

    initial commit

---
+++ src/main/java/com/tts/formValidationHW/FormValidationHwApplication.java
@@ -0,0 +1,13 @@
+package com.tts.formValidationHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidationHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormValidationHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/formValidationHW/FormValidationHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.formValidationHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormValidationHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
